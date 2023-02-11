b0dda27f3fb024521d974bcab5fc60db55ad71e9
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-02-11 20:33:34 +0900

    Upgrade @camome/*

--- camome.config.js
+++ camome.config.js
@@ -2,15 +2,8 @@ import { defineConfig } from "@camome/system";
 
 export default defineConfig({
   themes: {
-    light: {
-      transition: {
-        bg: "none",
-      },
-    },
-    dark: {
-      transition: {
-        bg: "none",
-      },
-    },
+    // Customize theme here.
+    light: {},
+    dark: {},
   },
 });
