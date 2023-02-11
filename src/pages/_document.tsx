6182a06ee25727b7bd78c715cda8dd86df06e9a3
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-02-11 20:16:27 +0900

    Tiny fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400..800&display=swap"
           rel="stylesheet"
-        ></link>
+        />
       </Head>
       <body>
         <Main />
