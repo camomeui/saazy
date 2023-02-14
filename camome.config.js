6c6e3f01e3ce1d9723498a4bf80aa823dc72ca21
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-02-14 12:25:44 +0900

    Enable web font (Inter)

--- camome.config.js
+++ camome.config.js
@@ -3,6 +3,13 @@ import { defineConfig } from "@camome/system";
 export default defineConfig({
   themes: {
     // Customize theme here.
+    common: {
+      font: {
+        family: {
+          base: `Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"`,
+        },
+      },
+    },
     light: {},
     dark: {},
   },
--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -8,33 +8,31 @@ import styles from "./styles.module.scss";
 export default function HeroSection() {
   return (
     <section className={styles.container}>
-      <div className={styles.left}>
-        <a href="#" className={styles.tagLink}>
-          <span className={styles.tagLink__tag}>New feature</span>
-          <span className={styles.tagLink__text}>Read the release note</span>
-          <ChevronRightIcon />
-        </a>
-        <div className={styles.mainCopy}>
-          <div>
-            Words to{" "}
-            <span className={styles.mainCopy__accent}>attract customers</span>.
-          </div>
-          <div>This is important, take it serious.</div>
+      <a href="#" className={styles.tagLink}>
+        <span className={styles.tagLink__tag}>New feature</span>
+        <span className={styles.tagLink__text}>Read the release note</span>
+        <ChevronRightIcon />
+      </a>
+      <div className={styles.mainCopy}>
+        <div>
+          Words to{" "}
+          <span className={styles.mainCopy__accent}>attract customers</span>.
         </div>
-        <div className={styles.subCopy}>
-          <Balancer>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et.
-          </Balancer>
-        </div>
-        <div className={styles.ctas}>
-          <Button component={Link} href="/signup">
-            Get started for free
-          </Button>
-          <Button component={Link} href="/contact" variant="soft">
-            Request demo
-          </Button>
-        </div>
+        <div>This is important, take it serious.</div>
+      </div>
+      <div className={styles.subCopy}>
+        <Balancer>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et.
+        </Balancer>
+      </div>
+      <div className={styles.ctas}>
+        <Button component={Link} href="/signup">
+          Get started for free
+        </Button>
+        <Button component={Link} href="/contact" variant="soft">
+          Request demo
+        </Button>
       </div>
     </section>
   );
