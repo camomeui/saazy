ea640d9eb100058cd97ee27c54b447062b9b1146
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-02-15 16:38:53 +0900

    Remove unused link tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,6 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="stylesheet" href="/styles/prism.css" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400..800&display=swap"
           rel="stylesheet"
